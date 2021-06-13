fb72888d0d9bb4c046f751080fe7097d0ad1be13
oogioogi <detacl73@gmail.com>
2021-06-13 14:36:48 +0900

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by 이용석 on 2021/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
